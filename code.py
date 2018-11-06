87be4fda56442abdbd114212a77654f04bc7ff05
Imad Pasha <prappleizerjohn@gmail.com>
2018-11-06 14:33:35 -0600

    add code

---
+++ code.py
@@ -0,0 +1,4 @@
+
+def do_something():
+    something = ' Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.'
+    print(something)# this will make it much easier in future problems to see that something is actually happening
